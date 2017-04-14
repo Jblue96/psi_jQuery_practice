903935b10ae3ab46b9ac790ffe275a27766c1faa
Jorge Blue <jblue5250@gmail.com>
2017-04-14 15:32:10 -0500

    red in square 1

--- practice_project/script.js
+++ practice_project/script.js
@@ -8,25 +8,41 @@
   you are also welcome to write more than the given functions.
 */
 var numberOfSquares = 10;
+var squareCount = 0;
 
 $(document).ready(onReady);
 
 //Function that runs when html is fully loaded:
 function onReady(){
   //click listener to button:
-  $('.change-highlight').on('click', highlightASquare)
-
+  $('.change-highlight').on('click', highlightASquare);
+// $('.square').append()
+// $('.container').append("<div class='square " + this.);
   //call to put starting squares on DOM
   appendSquaresToDom(numberOfSquares);
+}
+function addHlClass(){
+  $('.change-highlight').on('click', addClass);
+  // $("div").addClass('highlight');
+  console.log('addHlClass', addClass);
 }
 
 function highlightASquare(){
   //This variable stores an array of the elements with the 'square' class
   var arrayOfSquares = $('.square');
+  $("div:eq(1)").addClass('highlight');
+
+  // $('.square').append('arrayOfSquares');
+  // $('.square').addClass(".highlight",onReady([i],'.square'));
+  // $('div:').removeClass("square",addClass("highlight"));
 
 }
 
-
+function addSquareToArray(){
+console.log('square');
+// $('.square div').eq('i').addClass('.highlightASquare');
+$('.highlight:nth-of-type(1)');
+}
 //You don't need to change this function:
 function appendSquaresToDom(number){
   var squaresToAppend = "";
